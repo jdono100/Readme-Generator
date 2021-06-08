344b30c2d047a167f055d9e1048d46ac1971aa92
Jared Donovan <jdono100@gmail.com>
2021-06-07 20:16:17 -0500

    questions written out

--- index.js
+++ index.js
@@ -3,8 +3,64 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: chalk.red("What's the title of your project?"),
+        name: 'title',
+        default: 'Project-Title'
+    },
+    {
+        type: 'input',
+        message: chalk.red('Enter a description for the app'),
+        name: 'description',
+        default: 'Project description'
+    },
+    {
+        type: 'input',
+        message: chalk.blue('Enter your github username (no @ necessary).'),
+        name: 'gituser',
+        default: 'jaredonovan'
+    },
+    {
+        type: 'input',
+        message: chalk.blue('Enter your email address.'),
+        name: 'email',
+        default: 'jdono100@gmail.com'
+    },
+    {
+        type: 'input',
+        message: chalk.red('Enter the steps for installing the app.'),
+        name: 'installation',
+        default: 'Instructions for installing the app'
+    },
+    {
+        type: 'input',
+        message: chalk.red('Explain how to use the app and its real-world usefulness.'),
+        name: 'usage',
+        default: 'How to use the project and explanation for its usefulness'
+    },
+    {
+        type: 'input',
+        message: chalk.red('Explain procedures for contributing to the project.'),
+        name: 'contribution',
+        default: 'Guidelines for contributing to the project'
+    },
+    {
+        type: 'input',
+        message: chalk.red('Explain how to test the project.'),
+        name: 'test',
+        default: 'Instructions to test the app'
+    },
+    {
+        type: 'checkbox',
+        message: chalk.blue('Choose license(s) to protect the project.'),
+        choices: ['MIT', 'Apache 2.0', 'AGPLv3', 'GPLv3', 'LGPLv3', 'Mozilla 2.0'],
+        name: 'licenses',
+        default: 'MIT'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
